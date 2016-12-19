402dff1adda68fd3956986aa480e53950cebe368
imxieyi <wez733@live.cn>
2016-12-19 20:56:09 +0800

    Small modification
    In order to prevent duplicate include error.

--- main/include/adxl345.h
+++ main/include/adxl345.h
@@ -1,9 +1,9 @@
+#ifndef __ADXL345_H__
+#define __ADXL345_H__
 #include "stdint.h"
 #include "stdbool.h"
 
-#ifndef __ADXL345_H__
-#define __ADXL345_H__
-#endif
-
 void initAcc(uint8_t scl_pin, uint8_t sda_pin);
 void getAccelerometerData(int *result);
+
+#endif
